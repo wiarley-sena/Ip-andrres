4e486a6ec40d66b588bcbdda3619e040078e382f
wiarley sena <bruxosena03@gmail.com>
2026-05-12 10:04:02 -0300

    mudei resquet para pegar o ip do usuario e não o da vercel

--- src/app/api/ip/route.js
+++ src/app/api/ip/route.js
@@ -3,9 +3,11 @@ const porta = "http://ip-api.com/json";
 export async function GET(request) {
   try {
     const ipDigitado = request.nextUrl.searchParams.get("ip");
-    const url = ipDigitado ? `${porta}/${ipDigitado}`: porta
-      const resposta = await fetch(url);
-      const dados = await resposta.json();
+    const ipReal = request.headers.get("x-forwarded-for")?.split(",")[0];
+    const ipParaBuscar = ipDigitado || ipReal || ""
+    const url = ipParaBuscar ? `${porta}/${ipDigitado}`: porta
+    const resposta = await fetch(url);
+    const dados = await resposta.json();
       return new Response(JSON.stringify(dados))
   } catch {
     return new Response(null, { status: 500 });
